293910d94a961c534cf954388e078a6338e9e022
Thiago Borghi Lopes Galvão <thiago-galvao@thiago.galvao>
2020-09-30 11:38:59 -0300

    Data seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_category (name) VALUES('Books');
+INSERT INTO tb_category (name) VALUES('Eletronics');
+INSERT INTO tb_category (name) VALUES('Tools');
+INSERT INTO tb_category (name) VALUES('Eletronic Tools');
+INSERT INTO tb_category (name) VALUES('Hardware');
+INSERT INTO tb_category (name) VALUES('Toys');
+INSERT INTO tb_category (name) VALUES('Suits');
+INSERT INTO tb_category (name) VALUES('Accessories');
